5f30f53edb64f9f2c867237bed56c55e0721b339
SamatarCodes <thewind07@protonmail.com>
2020-10-19 19:04:53 -0400

    added popular tv shows

--- movie_app/script.js
+++ movie_app/script.js
@@ -1,9 +1,82 @@
-const movieContainer = document.getElementById('movie_container');
-const checkbox = document.getElementById('nav-toggle');
+//const key = require('./key');
 
-window.addEventListener('resize', () => {
-  if (document.body.clientWidth >= 885) {
-    checkbox.checked = false;
-    console.log('check');
+//const movieContainer = document.getElementById('movie_container');
+//const checkbox = document.getElementById('nav-toggle');
+const moviesContainer = document.getElementById('movies__container');
+const tvContainer = document.getElementById('tv_container');
+
+// window.addEventListener('resize', () => {
+//   if (document.body.clientWidth >= 885) {
+//     checkbox.checked = false;
+//     console.log('check');
+//   }
+// });
+
+// * fetch movies
+
+const apiKey = key(); //  REMOVE THIS FUNCTION AND ENTER YOUR API KEY HERE
+const popularMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+const popularTvShows = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`;
+const fetchMovies = async () => {
+  try {
+    const response = await fetch(popularMovies);
+    const responseTvShows = await fetch(popularTvShows);
+
+    const moviesArray = await response.json();
+    const tvArray = await responseTvShows.json();
+    console.log(tvArray);
+    displayPopularMovies(moviesArray.results);
+    displayPopularTvShows(tvArray.results);
+
+    //console.log(moviesArray);
+  } catch (err) {
+    console.log(err);
   }
-});
+};
+
+const displayPopularMovies = (moviesArray) => {
+  // loop through the movie array
+  moviesArray.forEach((movie) => {
+    // Create a movie div
+    const movieCard = document.createElement('div');
+    // Add movie class
+    movieCard.classList.add('movie');
+    // create the innerHTML for each movie
+    movieCard.innerHTML = `
+  <a href="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt=""
+        class="movie--image">
+  </a>
+          <h4 class="movie--title">${movie.original_title}</h4>
+          <span class="movie--genre"> ${movie.release_date}</span>
+  `;
+
+    // append it
+    moviesContainer.appendChild(movieCard);
+  });
+};
+
+const displayPopularTvShows = (tvArray) => {
+  // Loop through the tv array
+  tvArray.forEach((tv) => {
+    // for each tv show, do this below
+    // Create a movie div
+    const movieCard = document.createElement('div');
+    // Add movie class
+    movieCard.classList.add('movie');
+
+    movieCard.innerHTML = `
+    <a href="https://image.tmdb.org/t/p/w500/${tv.poster_path}">
+          <img src="https://image.tmdb.org/t/p/w500/${tv.poster_path}" alt=""
+          class="movie--image">
+    </a>
+            <h4 class="movie--title">${tv.original_name}</h4>
+            <span class="movie--genre"> ${tv.first_air_date}</span>
+    `;
+
+    // append to tv container
+    tvContainer.appendChild(movieCard);
+  });
+};
+
+fetchMovies();
