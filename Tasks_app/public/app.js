c70fa0c0a31ea351a0996c6cba21bae9af2a08f8
SamatarCodes <thewind07@protonmail.com>
2020-10-27 15:05:33 -0400

    able to add/delete tasks

--- Tasks_app/public/app.js
+++ Tasks_app/public/app.js
@@ -1,57 +1,66 @@
 // * Get all DOM references needed
 const input = document.getElementById('input');
 const todosContainer = document.getElementById('todos');
+const taskNumberEl = document.getElementById('taskNumber');
 
-// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-const todosArray = [];
-// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+let taskNumber = 0;
 
 // ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 const addTodo = (userInput) => {
   const todoEl = document.createElement('todo');
+
   todoEl.innerHTML = `
-    <div class="perTodo border-t border-gray-900 b py-2 flex justify-between items-center ">
+    <div class="perTodo opacity-100 animate-pulse border-t border-gray-900 b py-2 flex justify-between items-center transition-opacity ease-in duration-700 ">
         <div class="left-side">
-        <input type="checkbox" class="mr-2 checkbox"  />
-        <span class="text-gray-400 text-sm">${userInput}</span>
+        <label><input type="checkbox" class="mr-2 checkbox"  /></label>
+        <span class="text-gray-400 text-sm ">${userInput}</span>
         </div>
-        <button id="deleteBtn"><i class="far fa-trash-alt"></i></button>
-</div>
+        <button id="deleteBtn" class=""><i class="far fa-trash-alt"></i></button>
+    </div>
 `;
   // append
   todosContainer.appendChild(todoEl);
+  taskNumber++;
+  taskNumberEl.textContent = taskNumber;
 
-  // if checkbox is checked, add line-through
+  // * add event listener to todoEl
   todoEl.addEventListener('click', (e) => {
-    if (e.target.checked) {
-      todoEl.classList.add('addLineThrough');
-    } else {
-      todoEl.classList.remove('addLineThrough');
-    }
-    console.log(e.target);
-    // check if the delete button is clicked
-    if (e.target.classList.contains('fa-trash-alt')) {
-      // remove the element
-      todoEl.remove();
-    }
-  });
+    const spans = todoEl.querySelectorAll('span');
+    const checkboxes = todoEl.querySelectorAll('.checkbox');
 
-  // if trash bin is clicked, delete the item
-  //   todoEl.addEventListener('click', (e) => {
-  //     const removeTodo = e.target.parentElement.parentElement;
-  //     removeTodo.remove();
-  //   });
+    // loop through the checkbox and see if its checked
+    checkboxes.forEach((checkbox) => {
+      // if element clicked is a checkbox and its checked then...
+      if (e.target === checkbox && checkbox.checked) {
+        todoEl.classList.add('addLineThrough');
+        taskNumber--;
+        taskNumberEl.textContent = taskNumber;
+        /// if element clicked is a checkbox and its NOT checked then...
+      } else if (e.target === checkbox && !checkbox.checked) {
+        taskNumber++;
+        taskNumberEl.textContent = taskNumber;
+        todoEl.classList.remove('addLineThrough');
+      }
+
+      // * Delete button
+      if (e.target.classList.contains('fa-trash-alt')) {
+        console.log('click');
+        todoEl.remove();
+        if (!checkbox.checked) {
+          taskNumber--;
+          taskNumberEl.textContent = taskNumber;
+        }
+      }
+    });
+  });
 };
 
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 // * Input event listener
 input.addEventListener('keydown', (e) => {
   // if user presses on Enter key
-  if (e.key === 'Enter') {
-    // add user input to todosArray
-    // todosArray.push(e.target.value);
-    // call the addTodoToDom function to display it
-    //  addTodoToDom(e.target.value);
+  if (e.key === 'Enter' && e.target.value !== '') {
     addTodo(e.target.value);
     // clear input field
     e.target.value = '';
--- Tasks_app/tailwind.config.js
+++ Tasks_app/tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: 'pulse .8s cubic-bezier(0.4, 0, 0.6, 1)',
+        disappear: 'disappear .8s  cubic-bezier(0.4, 0, 0.6, 1)',
+      },
+      keyframes: {
+        pulse: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '.5' },
+          '100%': { opacity: '1' },
+        },
+        disappear: {
+          '0%': { opacity: '1' },
+          '50%': { opacity: '.5' },
+          '100%': { opacity: '0' },
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
