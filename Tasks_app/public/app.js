e493fc85bdffb128742afc7c57bd53a190aa29ee
SamatarCodes <thewind07@protonmail.com>
2020-11-02 13:08:44 -0500

    added some padding below input

--- Tasks_app/public/app.js
+++ Tasks_app/public/app.js
@@ -1,9 +1,9 @@
 // * Get all DOM references needed
 const input = document.getElementById('input');
 const todosContainer = document.getElementById('todos');
-const taskNumberEl = document.getElementById('taskNumber');
+const taskCounterEl = document.getElementById('taskNumber');
 
-let taskNumber = 0;
+let taskCounter = 0;
 
 // ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
@@ -11,7 +11,7 @@ const addTodo = (userInput) => {
   const todoEl = document.createElement('todo');
 
   todoEl.innerHTML = `
-    <div class="perTodo opacity-100 animate-pulse border-t border-gray-900 b py-2 flex justify-between items-center transition-opacity ease-in duration-700 ">
+    <div class="perTodo opacity-100 animate-pulse border-t border-gray-900 b py-2 flex justify-between items-center  ">
         <div class="left-side">
         <label><input type="checkbox" class="mr-2 checkbox"  /></label>
         <span class="text-gray-400 text-sm ">${userInput}</span>
@@ -21,8 +21,8 @@ const addTodo = (userInput) => {
 `;
   // append
   todosContainer.appendChild(todoEl);
-  taskNumber++;
-  taskNumberEl.textContent = taskNumber;
+  taskCounter++;
+  taskCounterEl.textContent = taskCounter;
 
   // * add event listener to todoEl
   todoEl.addEventListener('click', (e) => {
@@ -34,22 +34,23 @@ const addTodo = (userInput) => {
       // if element clicked is a checkbox and its checked then...
       if (e.target === checkbox && checkbox.checked) {
         todoEl.classList.add('addLineThrough');
-        taskNumber--;
-        taskNumberEl.textContent = taskNumber;
+        taskCounter--;
+        taskCounterEl.textContent = taskCounter;
         /// if element clicked is a checkbox and its NOT checked then...
       } else if (e.target === checkbox && !checkbox.checked) {
-        taskNumber++;
-        taskNumberEl.textContent = taskNumber;
+        taskCounter++;
+        taskCounterEl.textContent = taskCounter;
         todoEl.classList.remove('addLineThrough');
       }
 
       // * Delete button
       if (e.target.classList.contains('fa-trash-alt')) {
-        console.log('click');
+        todoEl.classList.toggle('animate-disappear');
         todoEl.remove();
+
         if (!checkbox.checked) {
-          taskNumber--;
-          taskNumberEl.textContent = taskNumber;
+          taskCounter--;
+          taskCounterEl.textContent = taskCounter;
         }
       }
     });
