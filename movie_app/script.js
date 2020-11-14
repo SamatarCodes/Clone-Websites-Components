5e07b3f71bf4fb25b7684fa4dc8bf8dc9f3f34e3
SamatarCodes <thewind07@protonmail.com>
2020-11-13 23:21:03 -0500

    grouped all api calls to a single class  and moved dom functions to utils file

--- movie_app/script.js
+++ movie_app/script.js
@@ -1,174 +1,54 @@
-const moviesContainer = document.getElementById('movies__container');
-const tvContainer = document.getElementById('tv_container');
-const searchInput = document.getElementById('search');
-const menuLi = document.querySelectorAll('.menu__list_item');
-
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-// * fetch movies
-
-const apiKey = key(); //  REMOVE THIS FUNCTION AND ENTER YOUR API KEY HERE
-const popularMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-const popularTvShows = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`;
-
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-const loadPopularMoviesAndTvs = async () => {
-  try {
-    // Fetch movies and tv shows
-    const response = await fetch(popularMovies);
-    const responseTvShows = await fetch(popularTvShows);
-
-    const moviesArray = await response.json();
-    console.log(moviesArray);
-    const tvArray = await responseTvShows.json();
-
-    // Call both movies and tv shows functions
-    displayPopularMovies(moviesArray.results);
-    displayPopularTvShows(tvArray.results);
-  } catch (err) {
-    console.log(err);
+class Movies {
+  constructor() {
+    this.key = key(); // ENTER YOUR OWN API KEY HERE
+    this.popularMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${this.key}&language=en-US&page=1`;
+    this.popularTvShows = `https://api.themoviedb.org/3/tv/popular?api_key=${this.key}&language=en-US&page=1`;
   }
-};
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-const fetchAnything = async (category, filter) => {
-  const data = `https://api.themoviedb.org/3/${category}/${filter}?api_key=${apiKey}&language=en-US&page=1`;
-  try {
-    const response = await fetch(data);
-    const resultsData = await response.json();
-    //console.log(resultsData.results);
+  async loadPopularMoviesAndTvs() {
+    try {
+      // Fetch movies and tv shows
+      const response = await fetch(this.popularMovies);
+      const responseTvShows = await fetch(this.popularTvShows);
 
-    if (category === 'movie') {
-      displayPopularMovies(resultsData.results);
-    } else if (category === 'tv') {
-      console.log(resultsData.results);
-      displayPopularTvShows(resultsData.results);
+      const moviesArray = await response.json();
+      //console.log(moviesArray);
+      const tvArray = await responseTvShows.json();
+
+      // Call both movies and tv shows functions
+      displayPopularMovies(moviesArray.results);
+      displayPopularTvShows(tvArray.results);
+    } catch (err) {
+      console.log(err);
     }
-  } catch (err) {
-    console.log(err);
   }
-};
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-const searchData = async (searchTerm) => {
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${searchTerm}`
-    );
-    const resultsData = await response.json();
-    return resultsData.results;
-  } catch (err) {
-    console.log(err);
+  async fetchAnything(category, filter) {
+    const data = `https://api.themoviedb.org/3/${category}/${filter}?api_key=${this.key}&language=en-US&page=1`;
+    try {
+      const response = await fetch(data);
+      const resultsData = await response.json();
+
+      if (category === 'movie') {
+        displayPopularMovies(resultsData.results);
+      } else if (category === 'tv') {
+        console.log(resultsData.results);
+        displayPopularTvShows(resultsData.results);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
-};
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-const createTvDom = (tv) => {
-  // Create a movie div
-  const movieCard = document.createElement('div');
-  // Add movie class
-  movieCard.classList.add('movie');
-
-  movieCard.innerHTML = `
-  <a href="https://image.tmdb.org/t/p/w500/${tv.poster_path}">
-        <img src="https://image.tmdb.org/t/p/w500/${tv.poster_path}" alt=""
-        class="movie--image">
-  </a>
-          <h4 class="movie--title">${tv.original_name}</h4>
-          <span class="movie--genre"> ${tv.first_air_date}</span>
-  `;
-
-  // append to tv container
-  tvContainer.appendChild(movieCard);
-};
-
-const createMovieDom = (movie) => {
-  // Create a movie div
-  const movieCard = document.createElement('div');
-  // Add movie class
-  movieCard.classList.add('movie');
-  // create the innerHTML for each movie
-  movieCard.innerHTML = `
-  <a href="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
-        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt=""
-        class="movie--image">
-  </a>
-          <h4 class="movie--title">${movie.original_title}</h4>
-          <span class="movie--genre"> ${movie.release_date}</span>
-  `;
-
-  // append it
-  moviesContainer.appendChild(movieCard);
-};
-
-//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-const displayPopularMovies = (moviesArray) => {
-  moviesContainer.innerHTML = '';
-  // loop through the movie array
-  moviesArray.forEach((movie) => {
-    createMovieDom(movie);
-  });
-};
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-const displayPopularTvShows = (tvArray) => {
-  tvContainer.innerHTML = '';
-  // Loop through the tv array
-  tvArray.forEach((tv) => {
-    createTvDom(tv);
-  });
-};
-
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-const onInput = async (e) => {
-  const userInput = e.target.value;
-  const results = await searchData(e.target.value); // returns array
-
-  // Clear movie container
-  moviesContainer.innerHTML = '';
-  // Clear tv container
-  tvContainer.innerHTML = '';
-
-  // loop through the results of search data
-  results.forEach((movie) => {
-    // for each  movie
-    if (movie.media_type === 'movie') {
-      createMovieDom(movie);
-    } else if (movie.media_type === 'tv') {
-      createTvDom(movie);
+  async searchData(searchTerm) {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/multi?api_key=${this.key}&language=en-US&page=1&include_adult=false&query=${searchTerm}`
+      );
+      const resultsData = await response.json();
+      return resultsData.results;
+    } catch (err) {
+      console.log(err);
     }
-  });
-  // Clear input field
-  e.target.value = '';
-};
-
-//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-// Search input
-searchInput.addEventListener('input', debounce(onInput, 500));
-
-// Sidebar  Menu
-menuLi.forEach((menu) => {
-  menu.addEventListener('click', () => {
-    switch (menu.textContent) {
-      case 'Popular Movies':
-        fetchAnything('movie', 'popular');
-        break;
-      case 'Now Playing':
-        fetchAnything('movie', 'now_playing');
-        break;
-      case 'Popular TV Shows':s
-        fetchAnything('tv', 'popular');
-        break;
-      case 'Airing Today':
-        fetchAnything('tv', 'airing_today');
-        break;
-      case 'Top Rated':
-        fetchAnything('tv', 'top_rated');
-        break;
-
-      default:
-        fetchAnything('movie', 'popular');
-    }
-  });
-});
-
-//Load movies
-loadPopularMoviesAndTvs();
+  }
+}
--- movie_app/utils.js
+++ movie_app/utils.js
@@ -1,3 +1,9 @@
+const moviesContainer = document.getElementById('movies__container');
+const tvContainer = document.getElementById('tv_container');
+const searchInput = document.getElementById('search');
+const menuLi = document.querySelectorAll('.menu__list_item');
+const moviesAndTvs = new Movies();
+
 // * Debounce function helper
 const debounce = (func, delay = 1000) => {
   let timeoutId;
@@ -17,3 +23,115 @@ const debounce = (func, delay = 1000) => {
     }, delay);
   };
 };
+
+const createTvDom = (tv) => {
+  // Create a movie div
+  const movieCard = document.createElement('div');
+  // Add movie class
+  movieCard.classList.add('movie');
+
+  movieCard.innerHTML = `
+    <a href="https://image.tmdb.org/t/p/w500/${tv.poster_path}">
+          <img src="https://image.tmdb.org/t/p/w500/${tv.poster_path}" alt=""
+          class="movie--image">
+    </a>
+            <h4 class="movie--title">${tv.original_name}</h4>
+            <span class="movie--genre"> ${tv.first_air_date}</span>
+    `;
+
+  // append to tv container
+  tvContainer.appendChild(movieCard);
+};
+
+const createMovieDom = (movie) => {
+  // Create a movie div
+  const movieCard = document.createElement('div');
+  // Add movie class
+  movieCard.classList.add('movie');
+  // create the innerHTML for each movie
+  movieCard.innerHTML = `
+    <a href="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+          <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt=""
+          class="movie--image">
+    </a>
+            <h4 class="movie--title">${movie.original_title}</h4>
+            <span class="movie--genre"> ${movie.release_date}</span>
+    `;
+
+  // append it
+  moviesContainer.appendChild(movieCard);
+};
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+const displayPopularMovies = (moviesArray) => {
+  moviesContainer.innerHTML = '';
+  // loop through the movie array
+  moviesArray.forEach((movie) => {
+    createMovieDom(movie);
+  });
+};
+
+const displayPopularTvShows = (tvArray) => {
+  tvContainer.innerHTML = '';
+  // Loop through the tv array
+  tvArray.forEach((tv) => {
+    createTvDom(tv);
+  });
+};
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+const onInput = async (e) => {
+  const userInput = e.target.value;
+  const results = await moviesAndTvs.searchData(e.target.value); // returns array
+
+  // Clear movie container
+  moviesContainer.innerHTML = '';
+  // Clear tv container
+  tvContainer.innerHTML = '';
+
+  // loop through the results of search data
+  results.forEach((movie) => {
+    // for each  movie
+    if (movie.media_type === 'movie') {
+      createMovieDom(movie);
+    } else if (movie.media_type === 'tv') {
+      createTvDom(movie);
+    }
+  });
+  // Clear input field
+  e.target.value = '';
+};
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// Search input
+searchInput.addEventListener('input', debounce(onInput, 500));
+
+// Sidebar  Menu
+menuLi.forEach((menu) => {
+  menu.addEventListener('click', () => {
+    switch (menu.textContent) {
+      case 'Popular Movies':
+        moviesAndTvs.fetchAnything('movie', 'popular');
+        break;
+      case 'Now Playing':
+        moviesAndTvs.fetchAnything('movie', 'now_playing');
+        break;
+      case 'Popular TV Shows':
+        moviesAndTvs.fetchAnything('tv', 'popular');
+        break;
+      case 'Airing Today':
+        moviesAndTvs.fetchAnything('tv', 'airing_today');
+        break;
+      case 'Top Rated':
+        moviesAndTvs.fetchAnything('tv', 'top_rated');
+        break;
+
+      default:
+        moviesAndTvs.fetchAnything('movie', 'popular');
+    }
+  });
+});
+
+//Load movies
+moviesAndTvs.loadPopularMoviesAndTvs();
