79c8f76b27d6077e2e3abb33ba0e1c20d5662073
SamatarCodes <thewind07@protonmail.com>
2020-11-16 23:55:49 -0500

    added a getMovieDetails method

--- movie_app/script.js
+++ movie_app/script.js
@@ -1,10 +1,23 @@
 class Movies {
   constructor() {
+    this.test = '';
     this.key = key(); // ENTER YOUR OWN API KEY HERE
     this.popularMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${this.key}&language=en-US&page=1`;
     this.popularTvShows = `https://api.themoviedb.org/3/tv/popular?api_key=${this.key}&language=en-US&page=1`;
   }
 
+  async getMovieDetails(movieId) {
+    const movieDetails = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.key}`;
+
+    try {
+      // Fetch movies and tv shows
+      const response = await fetch(movieDetails);
+      const movieInfo = await response.json();
+      return movieInfo;
+    } catch (err) {
+      console.log(err);
+    }
+  }
   async loadPopularMoviesAndTvs() {
     try {
       // Fetch movies and tv shows
@@ -12,7 +25,6 @@ class Movies {
       const responseTvShows = await fetch(this.popularTvShows);
 
       const moviesArray = await response.json();
-      //console.log(moviesArray);
       const tvArray = await responseTvShows.json();
 
       // Call both movies and tv shows functions
@@ -46,6 +58,19 @@ class Movies {
         `https://api.themoviedb.org/3/search/multi?api_key=${this.key}&language=en-US&page=1&include_adult=false&query=${searchTerm}`
       );
       const resultsData = await response.json();
+      return resultsData.results;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async searchById() {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/find/tt4566758?api_key=${this.key}&language=en-US&external_source=imdb_id`
+      );
+      const resultsData = await response.json();
+      console.log(resultsData);
       return resultsData.results;
     } catch (err) {
       console.log(err);
