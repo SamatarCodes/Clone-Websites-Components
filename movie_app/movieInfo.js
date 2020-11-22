e4eed18169e975481a6ee3f74c2cee4c33a1640a
SamatarCodes <thewind07@protonmail.com>
2020-11-22 02:27:11 -0500

    added tv cast/credit

--- movie_app/movieInfo.js
+++ movie_app/movieInfo.js
@@ -4,7 +4,6 @@ const movies = new Movies();
 // Grab movieId and tvId from local storage
 const getMovieId = localStorage.getItem('movieId');
 const getTvId = localStorage.getItem('tvId');
-
 // Load the DOM
 const createMovieDom = async (getInfo) => {
   const movieDetails = await getInfo;
@@ -15,38 +14,54 @@ const createMovieDom = async (getInfo) => {
 
   movieCard.innerHTML = `
     
-    <div class="movie--image">
-    <img src="https://image.tmdb.org/t/p/w500/${
-      movieDetails.poster_path
-    }" alt="">
-    </div>
-<div class="movie__description">
-    <div class="movie__release">
-        <!-- title -->
-        <div class="movie__title">
-            <h3 class="movie--name">${movieDetails.original_title}</h3>
-            <span class="star-vote"><i class="fas fa-star star"></i>${
-              movieDetails.vote_average
-            }</span>
-        </div>
-        <div class="movie__release--date">
-            <span>${new Date(movieDetails.release_date).toDateString()}</span> |
-            <span>Action, Adventure</span>
-        </div>
-    </div>
-    <div class="movie__bio">
-        <p>${movieDetails.overview}</p>
-    </div>
-</div>
+  <div class="movie__description">
+  <div class="movie__release">
+      <!-- title -->
+      <div class="movie__title">
+          <h3 class="movie--name">${movieDetails.original_title}</h3>
+          <span class="star-vote"><i class="fas fa-star star"></i>${
+            movieDetails.vote_average
+          }</span>
+      </div>
+      <div class="movie__release--date">
+          <span>${new Date(movieDetails.release_date).toDateString()}</span> |
+          <span>Action, Adventure</span>
+      </div>
+  </div>
+  <div class="movie__bio">
+      <p class="movie__bio--text">${movieDetails.overview}</p>
+      <div class="credit">
+          <span>Director:</span>
+          <span>Writer:</span>
+          <span>Cast: </span>
+      </div>
+  </div>
+  </div>
+  <div class="image__section">
+      <img class="movie--image" src="https://image.tmdb.org/t/p/w500/${
+        movieDetails.poster_path
+      }" alt="movie or tv image">
+  </div>
+
     `;
 
   movieContainer.appendChild(movieCard);
 };
 
 const createTVDom = async (getInfo) => {
-  const movieDetails = await getInfo;
+  // getInfo is an async so we wait to get the info back
+  const tvDetails = await getInfo;
 
-  console.log(movieDetails);
+  let createdBy;
+  // Check if we have a name of who created the show
+  if (tvDetails.movieInfo.created_by.length > 0) {
+    // if we have a name, assign it to createdBy
+    createdBy = tvDetails.movieInfo.created_by[0].name;
+  } else {
+    // if we do not have a name, set it as not listed
+    createdBy = 'Not listed';
+  }
+
   // Create a movie div
   const movieCard = document.createElement('div');
   // add movie class for styling
@@ -54,30 +69,38 @@ const createTVDom = async (getInfo) => {
 
   movieCard.innerHTML = `
       
-      <div class="movie--image">
-      <img src="https://image.tmdb.org/t/p/w500/${
-        movieDetails.poster_path
-      }" alt="">
-      </div>
   <div class="movie__description">
       <div class="movie__release">
           <!-- title -->
           <div class="movie__title">
-              <h3 class="movie--name">${movieDetails.name}</h3>
+              <h3 class="movie--name">${tvDetails.movieInfo.name}</h3>
            <span class="star-vote"><i class="fas fa-star star"></i>${
-             movieDetails.vote_average
+             tvDetails.movieInfo.vote_average
            }</span>
           </div>
           <div class="movie__release--date">
               <span>First Air Date: ${new Date(
-                movieDetails.first_air_date
+                tvDetails.movieInfo.first_air_date
               ).toDateString()}</span> |
-              <span>Action, Adventure</span>
+              <span>Fix this</span>
           </div>
       </div>
       <div class="movie__bio">
-          <p>${movieDetails.overview}</p>
+          <p class="movie__bio--text">${tvDetails.movieInfo.overview}</p>
+          <div class="credit">
+          <span>Director: ${createdBy}</span>
+          <div class="cast">
+          <span>Cast: ${tvDetails.creditInfo[0].name}, ${
+    tvDetails.creditInfo[1].name
+  },  ${tvDetails.creditInfo[2].name},  ${tvDetails.creditInfo[3].name}, </span>
+          </div>
       </div>
+      </div>
+  </div>
+  <div class="image__section">
+  <img class="movie--image" src="https://image.tmdb.org/t/p/w500/${
+    tvDetails.movieInfo.poster_path
+  }" alt="tv show poster" title="${tvDetails.movieInfo.name}">
   </div>
       `;
 
