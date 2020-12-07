20fab7ba8d81bdf750b79b4820739e57ad9ba916
SamatarCodes <thewind07@protonmail.com>
2020-12-07 18:17:15 -0500

    added  movie/tv credits

--- movie_app/movieInfo.js
+++ movie_app/movieInfo.js
@@ -7,6 +7,22 @@ const getTvId = localStorage.getItem('tvId');
 // Load the DOM
 const createMovieDom = async (getInfo) => {
   const movieDetails = await getInfo;
+  console.log(movieDetails);
+
+  // Destructuring the movieDetails
+  const {
+    original_title,
+    vote_average,
+    release_date,
+    overview,
+    poster_path,
+  } = movieDetails.movieInfo;
+
+  // Movie cast
+  const { name } = movieDetails.movieCredits.cast[0];
+  //console.log(movieDetails.movieCredits.cast[0].name);
+  console.log(name);
+
   // Create a movie div
   const movieCard = document.createElement('div');
   // add movie class for styling
@@ -18,29 +34,38 @@ const createMovieDom = async (getInfo) => {
   <div class="movie__release">
       <!-- title -->
       <div class="movie__title">
-          <h3 class="movie--name">${movieDetails.original_title}</h3>
-          <span class="star-vote"><i class="fas fa-star star"></i>${
-            movieDetails.vote_average
-          }</span>
+          <h3 class="movie--name">${original_title}</h3>
+          <span class="star-vote"><i class="fas fa-star star"></i>${vote_average}</span>
       </div>
       <div class="movie__release--date">
-          <span>${new Date(movieDetails.release_date).toDateString()}</span> |
+          <span>${new Date(release_date).toDateString()}</span> |
           <span>Action, Adventure</span>
       </div>
   </div>
   <div class="movie__bio">
-      <p class="movie__bio--text">${movieDetails.overview}</p>
+      <p class="movie__bio--text">${overview}</p>
       <div class="credit">
-          <span>Director:</span>
-          <span>Writer:</span>
-          <span>Cast: </span>
+          <span>Director: ${movieDetails.movieCredits.crew[0].name}</span>
+         
+          <span>Cast: ${
+            movieDetails.movieCredits.cast[0] !== undefined
+              ? movieDetails.movieCredits.cast[0].name
+              : ''
+          }, ${
+    movieDetails.movieCredits.cast[1] !== undefined
+      ? movieDetails.movieCredits.cast[2].name
+      : ''
+  }, ${
+    movieDetails.movieCredits.cast[2] !== undefined
+      ? movieDetails.movieCredits.cast[2].name
+      : ''
+  }
+  </span>
       </div>
   </div>
   </div>
   <div class="image__section">
-      <img class="movie--image" src="https://image.tmdb.org/t/p/w500/${
-        movieDetails.poster_path
-      }" alt="movie or tv image">
+      <img class="movie--image" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="movie or tv image">
   </div>
 
     `;
@@ -51,7 +76,19 @@ const createMovieDom = async (getInfo) => {
 const createTVDom = async (getInfo) => {
   // getInfo is an async so we wait to get the info back
   const tvDetails = await getInfo;
+  console.log(tvDetails.creditInfo);
+  console.log(tvDetails.creditInfo.length);
 
+  // Destructuring the tvdetails information
+  const {
+    name,
+    vote_average,
+    first_air_date,
+    overview,
+    poster_path,
+  } = tvDetails.movieInfo;
+
+  // TV director
   let createdBy;
   // Check if we have a name of who created the show
   if (tvDetails.movieInfo.created_by.length > 0) {
@@ -61,7 +98,7 @@ const createTVDom = async (getInfo) => {
     // if we do not have a name, set it as not listed
     createdBy = 'Not listed';
   }
-
+  // |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
   // Create a movie div
   const movieCard = document.createElement('div');
   // add movie class for styling
@@ -74,36 +111,40 @@ const createTVDom = async (getInfo) => {
           <!-- title -->
           <div class="movie__title">
               <h3 class="movie--name">${tvDetails.movieInfo.name}</h3>
-           <span class="star-vote"><i class="fas fa-star star"></i>${
-             tvDetails.movieInfo.vote_average
-           }</span>
+           <span class="star-vote"><i class="fas fa-star star"></i>${vote_average}</span>
           </div>
           <div class="movie__release--date">
               <span>First Air Date: ${new Date(
-                tvDetails.movieInfo.first_air_date
+                first_air_date
               ).toDateString()}</span> |
               <span>Fix this</span>
           </div>
       </div>
       <div class="movie__bio">
-          <p class="movie__bio--text">${tvDetails.movieInfo.overview}</p>
+          <p class="movie__bio--text">${overview}</p>
           <div class="credit">
           <span>Director: ${createdBy}</span>
-          <div class="cast">
-          <span>Cast: ${tvDetails.creditInfo[0].name}, ${
-    tvDetails.creditInfo[1].name
-  },  ${tvDetails.creditInfo[2].name},  ${tvDetails.creditInfo[3].name}, </span>
-          </div>
+          <span>Cast: ${tvDetails.creditInfo[0].name}, 
+          ${
+            tvDetails.creditInfo[1].name !== undefined
+              ? tvDetails.creditInfo[1].name
+              : ''
+          }, ${
+    tvDetails.creditInfo[2].name !== undefined
+      ? tvDetails.creditInfo[2].name
+      : ''
+  } </span>
+
       </div>
       </div>
   </div>
   <div class="image__section">
-  <img class="movie--image" src="https://image.tmdb.org/t/p/w500/${
-    tvDetails.movieInfo.poster_path
-  }" alt="tv show poster" title="${tvDetails.movieInfo.name}">
+  <img class="movie--image" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="tv show poster" title="${name}">
   </div>
       `;
 
+  // const creditCast = document.querySelector('.movie__bio');
+  // movieCard.appendChild(actorDiv);
   movieContainer.appendChild(movieCard);
 };
 
